ec4c790b570f222e51b15256e8e9807c1d16e7b3
zhangdexin <zhangdexin@czkj1010.com>
2023-12-26 15:09:20 +0800

    文件大小写

---
+++ silk-sys/src/skp_silk_bwexpander_32.rs
@@ -0,0 +1,29 @@
+pub fn skp_silk_bwexpander_32(ar: &mut [i32; 16], d: usize, chirp_q16: i32) {
+    let mut tmp_chirp_q16 = chirp_q16;
+    for i in 0..(d - 1) {
+        ar[i] = (ar[i] >> 16) * tmp_chirp_q16 as i16 as i32
+            + ((ar[i] & 0xffff) * tmp_chirp_q16 as i16 as i32 >> 16)
+            + ar[i]
+                * (if 16 == 1 {
+                    (tmp_chirp_q16 >> 1) + (tmp_chirp_q16 & 1)
+                } else {
+                    (tmp_chirp_q16 >> 16 - 1) + 1 >> 1
+                });
+        tmp_chirp_q16 = (chirp_q16 >> 16) * tmp_chirp_q16 as i16 as i32
+            + ((chirp_q16 & 0xffff) * tmp_chirp_q16 as i16 as i32 >> 16)
+            + chirp_q16
+                * (if 16 == 1 {
+                    (tmp_chirp_q16 >> 1) + (tmp_chirp_q16 & 1)
+                } else {
+                    (tmp_chirp_q16 >> 16 - 1) + 1 >> 1
+                });
+    }
+    ar[d - 1] = (ar[d - 1] >> 16) * tmp_chirp_q16 as i16 as i32
+        + ((ar[d - 1] & 0xffff) * tmp_chirp_q16 as i16 as i32 >> 16)
+        + ar[d - 1]
+            * (if 16 == 1 {
+                (tmp_chirp_q16 >> 1) + (tmp_chirp_q16 & 1)
+            } else {
+                (tmp_chirp_q16 >> 16 - 1) + 1 >> 1
+            });
+}
---
+++ silk-sys/src/skp_silk_nlsf_stabilize.rs
@@ -0,0 +1,62 @@
+use crate::{
+    skp_limit_32, skp_r_shift, skp_r_shift_round,
+    SKP_Silk_sort::SKP_Silk_insertion_sort_increasing_all_values,
+};
+
+pub fn skp_silk_nlsf_stabilize(mut nlsf_q15: &mut [i32], n_delta_min_q15: &[i32], len: usize) {
+    let mut loops = 0;
+    while loops < 20 {
+        let mut min_diff_q15 = nlsf_q15[0] - n_delta_min_q15[0];
+        let mut i_len = 0;
+        for i in 1..len {
+            let diff_q15 = nlsf_q15[i] - (nlsf_q15[i - 1] + n_delta_min_q15[i]);
+            if diff_q15 < min_diff_q15 {
+                min_diff_q15 = diff_q15;
+                i_len = i;
+            }
+        }
+        let diff_q15 = (1 << 15) - (nlsf_q15[len - 1] + n_delta_min_q15[len]);
+        if diff_q15 < min_diff_q15 {
+            min_diff_q15 = diff_q15;
+            i_len = len;
+        }
+        if min_diff_q15 >= 0 {
+            return;
+        }
+        if i_len == 0 {
+            nlsf_q15[0] = n_delta_min_q15[0];
+        } else if i_len == len {
+            nlsf_q15[len - 1] = (1 << 15) - n_delta_min_q15[len];
+        } else {
+            let mut min_center_q15 = 0;
+            for k in 0..i_len {
+                min_center_q15 += n_delta_min_q15[k];
+            }
+            min_center_q15 += skp_r_shift!(n_delta_min_q15[i_len], 1);
+            let mut max_center_q15 = 1 << 15;
+            for k in (i_len+1..=len).rev() {
+                max_center_q15 -= n_delta_min_q15[k];
+            }
+            max_center_q15 -= n_delta_min_q15[i_len] - skp_r_shift!(n_delta_min_q15[i_len], 1);
+            let center_freq_q15 = skp_limit_32!(
+                skp_r_shift_round!(nlsf_q15[i_len - 1] + nlsf_q15[i_len], 1),
+                min_center_q15,
+                max_center_q15
+            );
+            nlsf_q15[i_len - 1] = center_freq_q15 - skp_r_shift!(n_delta_min_q15[i_len], 1);
+            nlsf_q15[i_len] = nlsf_q15[i_len - 1] + n_delta_min_q15[i_len];
+        }
+        loops += 1;
+    }
+    if loops == 20 {
+        SKP_Silk_insertion_sort_increasing_all_values(&mut nlsf_q15, len);
+        nlsf_q15[0] = i32::max(nlsf_q15[0], n_delta_min_q15[0]);
+        for i in 1..len {
+            nlsf_q15[i] = i32::max(nlsf_q15[i], nlsf_q15[i - 1] + n_delta_min_q15[i]);
+        }
+        nlsf_q15[len - 1] = i32::min(nlsf_q15[len - 1], (1 << 15) - n_delta_min_q15[len as usize]);
+        for i in (0..=len - 2).rev() {
+            nlsf_q15[i] = i32::min(nlsf_q15[i], nlsf_q15[i + 1] - n_delta_min_q15[i + 1]);
+        }
+    }
+}
