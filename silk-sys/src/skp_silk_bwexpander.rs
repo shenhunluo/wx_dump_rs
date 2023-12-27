17f82cc18219d80f441693a4b8d77353b99a7d9a
zhangdexin <zhangdexin@czkj1010.com>
2023-12-27 16:09:47 +0800

    fmt

--- silk-sys/src/skp_silk_bwexpander.rs
+++ silk-sys/src/skp_silk_bwexpander.rs
@@ -1,14 +1,10 @@
 use crate::skp_r_shift_round;
 
-pub fn skp_silk_bwexpander(
-    ar: &mut [i16],
-    d: usize,
-    mut chirp_q16: i32,
-) {
+pub fn skp_silk_bwexpander(ar: &mut [i16], d: usize, mut chirp_q16: i32) {
     let chirp_minus_one_q16 = chirp_q16 - 65536;
     for i in 0..d - 1 {
-        ar[i] = skp_r_shift_round!(chirp_q16 * ar[i] as i32,16) as i16;
-        chirp_q16 += skp_r_shift_round!(chirp_q16 * chirp_minus_one_q16,16);
+        ar[i] = skp_r_shift_round!(chirp_q16 * ar[i] as i32, 16) as i16;
+        chirp_q16 += skp_r_shift_round!(chirp_q16 * chirp_minus_one_q16, 16);
     }
-    ar[d-1] = skp_r_shift_round!(chirp_q16 * ar[d-1] as i32,16) as i16;
+    ar[d - 1] = skp_r_shift_round!(chirp_q16 * ar[d - 1] as i32, 16) as i16;
 }
